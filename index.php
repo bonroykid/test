3f62857ab6d9dc655ccee240d25e55382c032488
Bonroykid <bonroykid@gmail.com>
2017-06-09 13:19:11 +0200

    nazev
    jen přepsaný název stránky

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <head>
         <meta charset=utf-8>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Průmyslové značení - Čistící pásky pro tamponový tisk</title>
+        <title>TEST</title>
         <!-- Load Roboto font -->
         <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Catamaran" rel="stylesheet">
